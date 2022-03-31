dceca0cb65020fcc0f941f54eb26088c7ec90d01
eamon831 <eamon831@gmail.com>
2022-03-31 12:19:11 +0600

    Latest

--- app/src/main/java/com/example/grammercheck/MainActivity.java
+++ app/src/main/java/com/example/grammercheck/MainActivity.java
@@ -28,7 +28,11 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
     EditText input;
@@ -70,23 +74,74 @@ public class MainActivity extends AppCompatActivity {
                        // Toast.makeText(MainActivity.this, response.toString(), Toast.LENGTH_SHORT).show();
                         try {
                             List<String> error=new ArrayList<String>();
+                            Map mp=new HashMap();
 
                             JSONObject respon=response.getJSONObject("response");
                             JSONArray errors=respon.getJSONArray("errors");
                             //Toast.makeText(MainActivity.this, Integer.toString(errors.length()), Toast.LENGTH_SHORT).show();
+                            if(errors.length()==0)
+                            {
+                                output.setText(text);
+                                return;
+                            }
 
 
                             for(int i=0;i<errors.length();i++)
                             {
                                 JSONObject SingleError= (JSONObject) errors.get(i);
                                 String s=SingleError.getString("bad");
+                                int x=SingleError.getInt("offset");
+                                mp.put(x,s);
                                 error.add(new String(s));
+
                                // Toast.makeText(MainActivity.this, s, Toast.LENGTH_SHORT).show();
                             }
                             SpannableStringBuilder builder = new SpannableStringBuilder();
+                            List<Integer> list=new ArrayList<Integer>();
 
+                            Set set=mp.entrySet();//Converting to Set so that we can traverse
+                            Iterator itr=set.iterator();
+                            while(itr.hasNext()){
+                                //Converting to Map.Entry so that we can get key and value separately
+                                Map.Entry entry=(Map.Entry)itr.next();
+                                int x= (int) entry.getKey();
+                                String s= (String) entry.getValue();
+                                for(int i=x;i>=0;i--)
+                                {
+                                    if(text.substring(i,i+s.length()).equals(s))
+                                    {
+                                        list.add(i);
+                                        break;
+                                    }
+                                }
 
-                            for(int i=0;i<text.length()&&!error.isEmpty();)
+                            }
+                            for(int i=0;i<text.length();)
+                            {
+                                if(i==list.get(0))
+                                {
+                                    String s=error.get(0);
+                                    SpannableString span=new SpannableString(s);
+                                    span.setSpan(new ForegroundColorSpan(Color.RED),0,s.length(),0);
+                                    builder.append(span);
+                                    list.remove(0);
+                                    error.remove(0);
+                                    i+=s.length();
+                                    continue;
+
+                                }
+                                String temp=text.substring(i,i+1);
+                                // Toast.makeText(MainActivity.this, temp, Toast.LENGTH_SHORT).show();
+                                SpannableString span=new SpannableString(temp);
+                                span.setSpan(new ForegroundColorSpan(Color.RED),0,0,0);
+                                builder.append(span);
+                                i++;
+
+                            }
+                            output.setText(builder);
+
+                           //Naive Logic
+                           /* for(int i=0;i<text.length()&&!error.isEmpty();)
                             {
                                 String s=error.get(0);
 
@@ -128,7 +183,7 @@ public class MainActivity extends AppCompatActivity {
 
                             }
 
-                            output.setText(builder);
+                            output.setText(builder);*/
 
 
 
